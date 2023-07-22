018e53b3f7e6edf470fadf6885fe1d90d49728ad
Umar6501 <136899700+Umar6501@users.noreply.github.com>
2023-07-22 14:06:34 +0500

    homework 37

---
+++ main.js
@@ -0,0 +1,109 @@
+/*darsda ko'riladigan Masalalar
+yechmasalaring hamm bo'ladi degandilar 
+birinchi ustoz.!!!*/
+
+// Begin 1
+// let a = 6;
+// let b = 4;
+// let S = a * b;
+// let P = 2 * (a + b);
+// console.log(S, P);
+
+// Begin 2
+// let n = 3.14;
+// let d = 6.86;
+// let L = n * d;
+// console.log(L);
+
+// Begin 3
+// let a = 7;
+// let V = a ** 3;
+// let S = (6 * a) ** 2;
+// console.log(V, S);
+
+// Begin 4
+// let a = 4;
+// let b = 5;
+// let c = 3;
+// let V = a * b * c;
+// let S = 2 * (a * b + b * c + a * c);
+// console.log(V, S);
+
+// Begin 5
+// let a = 58998887;
+// let b = 99;
+// let M = (a + b) / 2;
+// console.log(M);
+
+// Begin 6
+// let a = 2;
+// let b = 2;
+// let c = 4;
+// let C = a ** 2 + b ** 2;
+// let P = a + b + c;
+// console.log(C, P);
+
+// Begin 7
+// let r1 = 32;
+// let r2 = 24;
+// let n = 10;
+// let S1 = n * r1;
+// let S2 = n * r2;
+// let S = n * (r1 - r2);
+// console.log(S1, S2, S);
+
+// Begin 8
+// let n = 3.14;
+// let R = 12;
+// let L = 2 * n * R;
+// let S = n * R ** 2;
+// console.log(L, S);
+
+// Begin 9
+// let x1 = 10;
+// let y1 = 10;
+// let x2 = 5;
+// let y2 = 5;
+// let L = (x1 - x2) ** 2 + (y1 - y2) ** 2;
+// console.log(L);
+
+// Begin 10
+// let A = "b";
+// let B = "c";
+// let C = "a";
+// console.log(A, B, C);
+
+// Begin 11
+// let x = 9;
+// let y = 4 * (x - 3) ** 6 - 7 * (x - 3) ** 3 + 2;
+// console.log(y);
+
+// Begin 12
+// let a = 3;
+// let A = a ** 3;
+// let B = a ** 6;
+// let C = a ** 9;
+// let D = a ** 15;
+// console.log(A, B, C, D);
+
+//Begin 13
+// let Tf = 50;
+// let Tc = (Tf - 32) * (5 / 9);
+// console.log(Tc);
+
+// Begin 14
+// let X = 2000;
+// let Y = 4000;
+// let A = X * 2; //2 kg shokolad
+// let B = Y * 2; //2 kg konfet
+// console.log(A, B);
+
+//Begin 15
+// let X = 2000;
+// let Y = 4000;
+// let A = X * 2; //2 kg shokolad
+// let B = Y * 2; //2 kg konfet
+// let AllSumma = A + B;
+// console.log(AllSumma);
+
+//Integer ... lar ni qila olmadim(
